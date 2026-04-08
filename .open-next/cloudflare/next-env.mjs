c2fde6ec7239c12885cc8071507766af3c1e6d2a
root <root@localhost.localdomain>
2026-04-08 18:10:49 +0800

    Fix Pages deploy: copy worker.js to assets/_worker.js

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"SILICONFLOW_API_KEY":"sk-qntrbodhvnacnegvrkugqerkvngqkhgrpublfqkbzgeldyjn"};
+export const development = {"SILICONFLOW_API_KEY":"sk-qntrbodhvnacnegvrkugqerkvngqkhgrpublfqkbzgeldyjn"};
+export const test = {};
